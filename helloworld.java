40f4f394a615bc2b3f77494bc9e273bc3ca9a6f3
guruprasadkm2000 <107938971+guruprasadkm2000@users.noreply.github.com>
2022-12-05 11:41:15 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - 10th November 2022 - v3 - 08:31 PM - Triggered by WebHook ");
+    System.out.println("Hello World - 5th December 2022 11.41 AM IST");
     }
 }
