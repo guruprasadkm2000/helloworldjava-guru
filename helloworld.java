91e6142d35747c94c11f388a839fd818cc8b8e93
guruprasadkm2000 <107938971+guruprasadkm2000@users.noreply.github.com>
2022-12-05 11:59:58 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - 5th December 2022 11:41 AM IST");
+    System.out.println("Hello World - 5th December 2022 11:51 AM IST");
     }
 }
